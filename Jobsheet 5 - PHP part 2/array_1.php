b7b800e30336f0c8d79541aa64da77be561de3bb
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-09-25 13:37:23 +0700

    Jobsheet 5 - Praktikum 1

---
+++ Jobsheet 5 - PHP part 2/array_1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+</body>
+<?php
+$ListDosen = [
+    "Elok Nur Hamdana",
+    "Unggul Pemenang",
+    "Bagas Nugraha"
+];
+
+echo $ListDosen[2] . "<br>";
+echo $ListDosen[1] . "<br>";
+echo $ListDosen[0] . "<br>";
+
+?>
+
+</html>
