2432e1fa639729d51a70c155c23ecf227bb46db9
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-10-09 13:17:52 +0700

    Jobsheet 7 - Praktikum 2

---
+++ Jobsheet 7 - Form Proccessing/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array();
+
+if (empty($myArray)) {
+    echo "array tidak terdefinisi atau kosong";
+} else {
+    echo "array terdefinisi dan tidak kosong";
+}
+echo "<br>";
+if (empty($nonExistentArray)) {
+    echo "variable tidak terdefinisi atau kosong";
+} else {
+    echo "variable terdefinisi dan tidak kosong";
+}
