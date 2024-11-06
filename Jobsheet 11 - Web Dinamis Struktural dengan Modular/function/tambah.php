3a95d4b391459ef40db8b361f0e63e7daac8cfa1
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-11-06 13:54:49 +0700

    update tambah.php

--- Jobsheet 11 - Web Dinamis Struktural dengan Modular/function/tambah.php
+++ Jobsheet 11 - Web Dinamis Struktural dengan Modular/function/tambah.php
@@ -4,8 +4,15 @@ if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../function/anti_injection.php';
     require '../function/pesan_kilat.php';
-    if(!empty($_GET['jabatan'])){
+    if (!empty($_GET['jabatan'])) {
         $jabatan = anti_injection($koneksi, $_GET['jabatan']);
         $keterangan = anti_injection($koneksi, $_GET['keterangan']);
+        $query = "INSERT INTO jabatan (jabatan, keterangan) VALUES ('$jabatan', '$keterangan')";
+        if (mysqli_query($koneksi, $query)) {
+            pesan('success', 'Jabatan berhasil ditambahkan');
+        } else {
+            pesan('danger', 'Jabatan gagal ditambahkan karena:' . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=jabatan");
     }
 }
