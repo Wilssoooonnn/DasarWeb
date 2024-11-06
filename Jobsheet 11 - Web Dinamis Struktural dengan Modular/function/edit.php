2424b0a29a30f7a39395d19bae1370bdbcc71aa0
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-11-06 15:05:35 +0700

    update anggota view, and add edit,hapus for anggota

--- Jobsheet 11 - Web Dinamis Struktural dengan Modular/function/edit.php
+++ Jobsheet 11 - Web Dinamis Struktural dengan Modular/function/edit.php
@@ -16,5 +16,44 @@ if (!empty($_SESSION['username'])) {
             pesan('danger', 'Jabatan gagal diubah karena:' . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])) {
+        $user_id = anti_injection($koneksi, $_POST['id']);
+        $nama = anti_injection($koneksi, $_POST['nama']);
+        $jabatan = anti_injection($koneksi, $_POST['jabatan']);
+        $jenis_kelamin = anti_injection($koneksi, $_POST['jenis_kelamin']);
+        $alamat = anti_injection($koneksi, $_POST['alamat']);
+        $no_telp = anti_injection($koneksi, $_POST['no_telp']);
+        $username = anti_injection($koneksi, $_POST['username']);
+        $query_anggota = "UPDATE anggota SET nama = '$nama',
+                                             jabatan_id = '$jabatan',
+                                             jenis_kelamin = '$jenis_kelamin',
+                                             alamat = '$alamat',
+                                             no_telp = '$no_telp'
+                                             WHERE user_id = '$user_id'";
+
+        if (mysqli_query($koneksi, $query_anggota)) {
+            if (!empty($_POST['password'])) {
+                $password = $_POST['password'];
+                $salt = bin2hex(random_bytes(16));
+                $combined_password = $salt . $password;
+                $hashed_password = password_hash($password . $salt, PASSWORD_BCRYPT);
+                $query_user = "UPDATE user SET username = '$username', password = '$hashed_password', salt = '$salt' WHERE id = '$user_id'";
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', 'Anggota berhasil diubah');
+                } else {
+                    pesan('danger', 'Anggota gagal diubah karena:' . mysqli_error($koneksi));
+                }
+            } else {
+                $query_user = "UPDATE user SET username = '$username' WHERE id = '$user_id'";
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', 'Anggota berhasil diubah');
+                } else {
+                    pesan('danger', 'Anggota gagal diubah karena:' . mysqli_error($koneksi));
+                }
+            }
+        } else {
+            pesan('danger', 'Anggota gagal diubah karena:' . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
     }
 }
